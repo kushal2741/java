85a19c718493ec7917346c907772f47683f1721f
Kushal <kushal.bansal@tothenew.com>
2018-10-01 06:16:59 +0530

    Updated code

--- src/com/company/daythree/Quesone.java
+++ src/com/company/daythree/Quesone.java
@@ -1,4 +1,81 @@
 package com.company.daythree;
 
 public class Quesone {
+    public static void main(String[] args) {
+        Printer print = new Printer();
+        Thread t1 = new Thread(new EvenOdd(print, 20, false));
+        Thread t2 = new Thread(new EvenOdd(print, 20, true));
+        t1.start();
+        t2.start();
+    }
 }
+class EvenOdd implements Runnable {
+
+    private int max;
+    private Printer print;
+    private boolean isEvenNumber;
+
+    EvenOdd(Printer print, int max, boolean isEvenNumber) {
+        this.print = print;
+        this.max = max;
+        this.isEvenNumber = isEvenNumber;
+    }
+
+    @Override
+    public void run() {
+
+        //System.out.println("Run method");
+        int number = isEvenNumber == true ? 2 : 1;
+        while (number <= max) {
+
+            if (isEvenNumber) {
+                //System.out.println("Even :"+ Thread.currentThread().getName());
+                print.printEven(number);
+                //number+=2;
+            } else {
+                //System.out.println("Odd :"+ Thread.currentThread().getName());
+                print.printOdd(number);
+                // number+=2;
+            }
+            number += 2;
+        }
+
+    }
+
+}
+
+class Printer {
+
+    boolean isOdd = false;
+
+    synchronized void printEven(int number) {
+
+        while (isOdd == false) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Even:" + number);
+        isOdd = false;
+        //to notify all waiting threads that the current thread is done with the job
+        notifyAll();
+    }
+
+    synchronized void printOdd(int number) {
+        while (isOdd == true) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Odd:" + number);
+        isOdd = true;
+        //to notify all waiting threads that the current thread is done with the job
+        notifyAll();
+    }
+
+}
+
