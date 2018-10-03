356cfb65cf1b7dcd11cdff82f4ffa9dd9a31475d
Kushal <kushal.bansal@tothenew.com>
2018-10-03 18:51:55 +0530

    Updated code for design pattern Piza question

--- src/com/company/DesignPatterns/PizzaStore.java
+++ src/com/company/DesignPatterns/PizzaStore.java
@@ -1,4 +1,81 @@
 package com.company.DesignPatterns;
 
-public class PizzaStore {
+
+abstract class Pizza
+{
+
+    String description = "Unkknown Pizza";
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public abstract int getCost();
+}
+
+
+abstract class ToppingsDecorator extends Pizza
+{
+    public abstract String getDescription();
+}
+
+// Concrete pizza classes
+
+class FarmHouse extends Pizza
+{
+    public FarmHouse() { description = "FarmHouse"; }
+    public int getCost() { return 200; }
+}
+class Margherita extends Pizza
+{
+    public Margherita() { description = "Margherita"; }
+    public int getCost() { return 100; }
+}
+
+
+// Concrete toppings classes
+class FreshTomato extends ToppingsDecorator
+{
+    // we need a reference to obj we are decorating
+    Pizza pizza;
+
+    public FreshTomato(Pizza pizza) { this.pizza = pizza; }
+    public String getDescription() {
+        return pizza.getDescription() + ", Fresh Tomato ";
+    }
+    public int getCost() { return 40 + pizza.getCost(); }
+}
+class Paneer extends ToppingsDecorator
+{
+    Pizza pizza;
+    public Paneer(Pizza pizza) { this.pizza = pizza; }
+    public String getDescription() {
+        return pizza.getDescription() + ", Paneer ";
+    }
+    public int getCost() { return 70 + pizza.getCost(); }
+}
+
+class PizzaStore
+{
+    public static void main(String args[])
+    {
+        // create new margherita pizza
+        Pizza pizza = new Margherita();
+        System.out.println( pizza.getDescription() +
+                " Cost :" + pizza.getCost());
+
+        // create new FarmHouse pizza
+        Pizza pizza2 = new FarmHouse();
+
+        // decorate it with freshtomato topping
+        pizza2 = new FreshTomato(pizza2);
+
+        //decorate it with paneer topping
+        pizza2 = new Paneer(pizza2);
+
+        System.out.println(pizza2.getDescription() +
+                " Cost :" + pizza2.getCost());
+
+    }
 }
